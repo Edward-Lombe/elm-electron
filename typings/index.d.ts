c062031e20fbecad63c01ae769c2e3d3e8183873
Edward Lombe <edward.lombe@mycosystems.com.au>
2016-07-09 11:28:10 +1200

    Added typings to source control

---
+++ typings/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="globals/github-electron/electron-prebuilt/index.d.ts" />
+/// <reference path="globals/github-electron/index.d.ts" />
+/// <reference path="globals/koa-mount/index.d.ts" />
+/// <reference path="globals/koa-static/index.d.ts" />
+/// <reference path="globals/koa/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/debug/index.d.ts" />
